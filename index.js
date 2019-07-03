159bea5bd3fd70d7e589bf8d8a9890d4e54eb86e
Nate Clark <n8@n8io.com>
2019-07-02 22:18:34 -0400

    🎉 Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const chokidar = require('chokidar');
+const got = require('got');
+
+const sendPath = async path => {
+  try {
+    await got.post({
+      url: 'http://localhost:1981',
+      body: { path: `/${path}` },
+      json: true
+    });
+  } catch (e) {
+    console.error('Error while sending request');
+    console.error(e);
+  }
+};
+
+const log = msg => {
+  const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZone: 'America/New_York'
+  };
+
+  const dt = new Intl.DateTimeFormat('en-US', options).format(new Date());
+
+  console.log(`${dt}: ${msg}`);
+};
+
+// One-liner for current directory, ignores .dotfiles
+chokidar.watch('/mnt/x/decrypted/**/*.(mkv|mp4|avi|n8)').on('unlink', path => {
+  log(path);
+  sendPath(path);
+});
