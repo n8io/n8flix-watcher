67ac0f034b159ab2117ae0ae7240c3640596fd36
Nate Clark <n8@n8io.com>
2019-07-03 08:33:20 -0400

    ✨ Make it more configurable

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const chokidar = require('chokidar');
 const got = require('got');
 
-const sendPath = async path => {
+const {
+  DRY_RUN = '',
+  HOST = 'plex.n8io.com',
+  PATHNAME = '',
+  PROTOCOL = 'http',
+  PORT = '1981',
+  WATCH_DIR = '/mnt/x/decrypted/**/*.(mkv|mp4|avi|n8)'
+} = process.env;
+
+const uri = `${PROTOCOL}://${HOST}:${PORT}/${PATHNAME}`;
+
+const sendPath = async tmpPath => {
+  let path = tmpPath;
+
+  if (tmpPath && !tmpPath.startsWith('/')) {
+    path = `/${tmpPath}`;
+  }
+
   try {
     await got.post({
-      url: 'http://plex.n8io.com:1981',
-      body: { path },
+      url: uri,
+      body: { debug: DRY_RUN ? true : undefined, path },
       json: true
     });
   } catch (e) {
@@ -31,7 +48,10 @@ const log = msg => {
 };
 
 // One-liner for current directory, ignores .dotfiles
-chokidar.watch('/mnt/x/decrypted/**/*.(mkv|mp4|avi|n8)').on('unlink', path => {
+chokidar.watch(WATCH_DIR).on('unlink', path => {
   log(path);
   sendPath(path);
 });
+
+log(`Paths will be posted to ${uri}`);
+log(`Watcher started for ${WATCH_DIR} ...`);
